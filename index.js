1039f5d273c0b74e7edd39a7992811363943d6c9
olivier <olivier@bodeswell.io>
2021-04-17 15:48:51 -0400

    4

--- index.js
+++ index.js
@@ -12,12 +12,22 @@ client.once('ready', () => {
 });
 
 client.on('guildMemberAdd', member => {
-    member.send("Welcome!");
-});
-
-client.on('message', message => {
-	console.log(message.content);
-});
+	// To compare, we need to load the current invite list.
+	member.guild.fetchInvites().then(guildInvites => {
+	  // This is the *existing* invites for the guild.
+	  const ei = invites[member.guild.id];
+	  // Update the cached invites for the guild.
+	  invites[member.guild.id] = guildInvites;
+	  // Look through the invites, find the one for which the uses went up.
+	  const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+	  // This is just to simplify the message being sent below (inviter doesn't have a tag property)
+	  const inviter = client.users.get(invite.inviter.id);
+	  // Get the log channel (change to your liking)
+	  const logChannel = member.guild.channels.find(channel => channel.name === "join-logs");
+	  // A real basic message with the information we need. 
+	  logChannel.send(`${member.user.tag} joined using invite code ${invite.code} from ${inviter.tag}. Invite was used ${invite.uses} times since its creation.`);
+	});
+  });
 
 client.on('message', async message => {
 	// Join the same voice channel of the author of the message

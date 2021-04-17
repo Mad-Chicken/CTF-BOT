5fb1e1c0781cd24e97093e65e77146945717e0da
olivier <olivier@bodeswell.io>
2021-04-17 15:12:04 -0400

    foo

--- index.js
+++ index.js
@@ -30,8 +30,7 @@ client.on("guildMemberRemove", member => {
 });
 */
 client.on('guildMemberAdd', member => {
-    member.roles.add(member.guild.roles.cache.find(i => i.name === 'Among The Server'))
-
+	console.log("joined")
     const welcomeEmbed = new Discord.MessageEmbed()
 
     welcomeEmbed.setColor('#5cf000')
@@ -42,8 +41,9 @@ client.on('guildMemberAdd', member => {
 })
 
 client.on('guildMemberRemove', member => {
+	console.log('someone left')
     const goodbyeEmbed = new Discord.MessageEmbed()
-
+	
     goodbyeEmbed.setColor('#f00000')
     goodbyeEmbed.setTitle('**' + member.user.username + '** was not the impostor there are **' + member.guild.memberCount + '** left Among Us')
     goodbyeEmbed.setImage('https://gamewith-en.akamaized.net/article/thumbnail/rectangle/22183.png')

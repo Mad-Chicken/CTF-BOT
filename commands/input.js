b3525e1851548cdc1c013dd53bf10370eedcc55e
olivier <olivier@bodeswell.io>
2021-04-17 17:31:39 -0400

    list with flags

--- commands/input.js
+++ commands/input.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const fs = require('fs');
 module.exports = {
 	name: 'input',
     description: 'input flag',
@@ -6,6 +7,17 @@ module.exports = {
 	usage: '<flag>',
 	execute(message, args) {
 		message.channel.send(` : ${args}`);
+        fs.readFile('./databases.json', 'utf8', (err, data) => {
+            if (err) {
+                console.log(`Error reading file from disk: ${err}`);
+            } else {     
+                // parse JSON string to JSON object
+                const databases = JSON.parse(data);
+                databases.forEach(db => {
+                    console.log(`${db}`);
+                });
+            }
+        });
         const mentionHook = new Discord.WebhookClient("833088941166952489", "qCLEk2Tm8qRZs3Qph0wR4-ghFvbHfWgOEiCRHZDaSMliS-rcgWWTXB4d65ZbLLJ5lQ3x");
         mentionHook.send("You were mentioned!");
 	},

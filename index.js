ad621dde1621f2cf974e41e3765f14d87b6ac47f
olivier <olivier@bodeswell.io>
2021-04-17 15:25:36 -0400

    foo

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ client.once('ready', () => {
 	client.user.setActivity("");
 });
 
+client.on("guildMemberAdd", (member) => {
+	console.log(`New User "${member.user.username}" has joined "${member.guild.name}"` );
+	member.guild.channels.find(c => c.name === "welcome").send(`"${member.user.username}" has joined this server`);
+});
+
+/*
 //Member Join
 client.on("guildMemberAdd", (member) => {
 	console.log(`New User "${member.user.username}" has joined "${member.guild.name}"` );
@@ -25,29 +31,6 @@ client.on("guildMemberRemove", member => {
 		console.log(`${member} left`)
 	});
 });
-/*
-client.on('guildMemberAdd', member => {
-	console.log("joined")
-    const welcomeEmbed = new Discord.MessageEmbed()
-
-    welcomeEmbed.setColor('#5cf000')
-    welcomeEmbed.setTitle('**' + member.user.username + '** is now Among Us other **' + member.guild.memberCount + '** people')
-    welcomeEmbed.setImage('https://cdn.mos.cms.futurecdn.net/93GAa4wm3z4HbenzLbxWeQ-650-80.jpg.webp')
-
-    member.guild.channels.cache.find(i => i.name === 'greetings').send(welcomeEmbed)
-})
-
-client.on('guildMemberRemove', member => {
-	console.log('someone left')
-    const goodbyeEmbed = new Discord.MessageEmbed()
-	
-    goodbyeEmbed.setColor('#f00000')
-    goodbyeEmbed.setTitle('**' + member.user.username + '** was not the impostor there are **' + member.guild.memberCount + '** left Among Us')
-    goodbyeEmbed.setImage('https://gamewith-en.akamaized.net/article/thumbnail/rectangle/22183.png')
-
-    member.guild.channels.cache.find(i => i.name === 'greetings').send(goodbyeEmbed)
-})
-*/
 
 //Open all commands in ./commands dir
 for (const file of commandFiles) {
@@ -169,5 +152,5 @@ client.on("messageReactionRemove", async (reaction, user) => {
 		}
 	}
 });
-
+*/
 client.login(token);

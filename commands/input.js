29be8610d8912eb78eca819c5b57d503759230ff
olivier <olivier@bodeswell.io>
2021-04-17 17:25:06 -0400

    fix

--- commands/input.js
+++ commands/input.js
@@ -4,8 +4,8 @@ module.exports = {
 	args: true,
 	usage: '<flag>',
 	execute(message, args) {
-		message.channel.send(`Setting status to: ${args}`);
-        const mentionHook = new Discord.WebhookClient("833088941166952489", "qCLEk2Tm8qRZs3Qph0wR4-ghFvbHfWgOEiCRHZDaSMliS-rcgWWTXB4d65ZbLLJ5lQ3x");
+		message.channel.send(` : ${args}`);
+        const mentionHook = new WebhookClient("833088941166952489", "qCLEk2Tm8qRZs3Qph0wR4-ghFvbHfWgOEiCRHZDaSMliS-rcgWWTXB4d65ZbLLJ5lQ3x");
         mentionHook.send("You were mentioned!");
 	},
 };

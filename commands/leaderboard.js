09ad98cb1bdf83251d1e01261696667d0ab64ce3
olivier <olivier@bodeswell.io>
2021-04-19 15:59:36 -0400

    work!

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -51,13 +51,13 @@ module.exports = {
 		  message.channel.send("", { embed });
 
 		for (let user in leaderboard_array_sorted) {
-			console.log(`${user[0]}  ${user[1]}`);
+			console.log(`${leaderboard_array_sorted[user][0]}  ${[user][1]}`);
 			message.channel.send({embed: {
 				color: 'RANDOM',
 		//		description: "-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-",
 				fields: [{
-					name: `<@${user[0]}>`,
-					value: `Flags collected: ${user[1]}`
+					name: `<@${leaderboard_array_sorted[user][0]}>`,
+					value: `Flags collected: ${leaderboard_array_sorted[user][1]}`
 				  }
 				]
 			  }

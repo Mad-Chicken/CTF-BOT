71d0460fa1fb9944c1948582ba252a46f985e184
olivier <olivier@bodeswell.io>
2021-04-19 15:33:45 -0400

    fooba

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -16,19 +16,65 @@ module.exports = {
         }
 		console.log(users_raw);
 		var users = JSON.parse(users_raw);
-		let output_data = "Leaderboard";
+//		let output_data = "Leaderboard";
+		let leaderboard_array = [];
 		for (let user in users) {
             if (users.hasOwnProperty(user)) {     
 				console.log(`[~] ${user} ${users[user]}`);      
                 console.log(`[~] ${user} ${users[user].length}`);
-				output_data += `\n${user}\t${users[user].length}`;
+				leaderboard_array.push([user, users[user].length]);
+//				output_data += `\n${user}\t${users[user].length}`;
+//	.sort().reverse();
             }
         }
+		console.log(leaderboard_array);
+		leaderboard_array.sort().reverse();
+		console.log(leaderboard_array);
+		
+		const embed = {
+			"title": "LEADERBOARD",
+			"description": "-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-",
+			"url": "https://discordapp.com",
+			"color": 10977743,
+			"timestamp": new Date(),
+			"footer": {
+			  "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png",
+			  "text": "footer text"
+			},
+			"thumbnail": {
+			  "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+			},
+			"image": {
+			  "url": "https://cdn.discordapp.com/embed/avatars/0.png"
+			},
+			"author": {
+			  "name": "author name",
+			  "url": "https://discordapp.com",
+			  "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
+			}
+		  };
+		  channel.send("", { embed });
+
+		for (let user in users) {
+			message.channel.send({embed: {
+				color: 'RANDOM',
+		//		description: "-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-",
+				fields: [{
+					name: `<@${user[0]}>`,
+					value: `Flags collected: ${user[1]}`
+				  }
+				]
+			  }
+			});
+        }
+
+/*
 		message.channel.send(`
 		${output_data}
 		
 		`).then(sentEmbed => {
 		    sentEmbed.react("🚱")
 		});
+*/
     },
 };

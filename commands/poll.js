2bc3611f7222b3551cde43d6c2fb910d57596db6
Mad-Chicken <legolemur@gmail.com>
2021-05-15 02:21:07 +0100

    f

--- commands/poll.js
+++ commands/poll.js
@@ -1,23 +1,38 @@
 const Discord = require('discord.js');
 module.exports = {
         name: 'poll',
-        aliases: ['remove', 'slap'],
+        aliases: ['vote'],
         devRole: 'true',
         guildOnly: true,
         args: true,
         description: 'Create a poll',
-        usage: '[<message>] <option-0> <option-1> <option-2> <option-n>',
+        usage: '[<message>] [<option-0>] [<option-1>] [<option-2>] [<option-n>]',
         execute(message, args) {
-            console.log(args)
-            console.logs(typeof args)
-            /*
-            const exampleEmbed = new Discord.MessageEmbed()
-                .setColor('RANDOM')
-                .setTitle('Join Us!')
-                .setDescription('ya you')
-                .setThumbnail('https://cdn.discordapp.com/icons/443885514207264779/846fae7d8bd3ae3dd75cb4f0f77bd550.png?size=256')
-                .setTimestamp()
-            message.channel.send(exampleEmbed)
-            */
+	    const pollMessage = args[0];
+	    const options = [];
+		message.delete();
+		const alfa = ["🇦","🇧""""🇨","d","e","f","g","h","i","j","k"];
+		args.shift();
+		pollMSG = "";
+	    for (const [i, value] of args.entries()) {
+		console.log(`${i} : ${alfa[i]}`);
+	        options.push(alfa[i]);
+		pollMSG += `${alfa[i]} : ${value}\n`;
+	    }
+		console.log(pollMSG);
+            const embed = {
+			"title": `**${pollMessage}**`,
+			"timestamp": new Date(),
+			"thumbnail": {
+				"url": "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fclipground.com%2Fimages%2Fpoll-clipart-6.jpg&f=1&nofb=1"
+			},
+			"description": pollMSG
+		};
+		message.channel.send("", { embed }).then(embedMessage => {
+			for (const emoji in options) {
+				console.log(options[emoji]);
+				embedMessage.react(options[emoji]);
+			}
+		});
         },
 };

3866d020af5cdef68754f72bce98a93b3308b662
olivier <olivier@bodeswell.io>
2021-05-24 13:21:46 -0400

    remove comment

--- commands/status.js
+++ commands/status.js
@@ -7,25 +7,21 @@ module.exports = {
 	usage: '<status>',
 	execute(message, args) {
 		if (args.join(' ') == "online") {
-			//message.client.user.setStatus('online');
 			message.client.user.setPresence({
 				status: 'online'
 			});
 			console.log(`Setting status to: ${args.join(' ')}`);
 		} else if (args.join(' ') == "dnd") {
-			//message.client.user.setStatus('dnd');
 			console.log(`Setting status to: ${args.join(' ')}`);
 			message.client.user.setPresence({
 				status: 'dnd'
 			});
 		} else if (args.join(' ') == "idle") {
-			//message.client.user.setStatus('idle');
 			console.log(`Setting status to: ${args.join(' ')}`);
 			message.client.user.setPresence({
 				status: 'idle'
 			});
 		} else if (args.join(' ') == "invisible") {
-			//message.client.user.setStatus('invisible');
 			console.log(`Setting status to: ${args.join(' ')}`);
 			message.client.user.setPresence({
 				status: 'invisible'
